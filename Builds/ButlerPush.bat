1c0207f41a5653a9b035e617124b4944b6003bec
josfeldmann <37274482+josfeldmann@users.noreply.github.com>
2022-10-02 20:38:54 -0400

    build changes

--- Builds/ButlerPush.bat
+++ Builds/ButlerPush.bat
@@ -1,5 +1,6 @@
-butler push PictoPull-Windows.zip josfeld/pictopull:windows
-butler push PictoPull-OSX.zip josfeld/pictopull:osx
-butler push PictoPull-Linux.zip josfeld/pictopull:linux
+butler push Alone-Windows.zip screensmith/alone:windows
+butler push Alone-OSX.zip screensmith/alone:osx
+butler push Alone-Linux.zip screensmith/alone:linux
+butler push Alone-WebGL.zip screensmith/alone:webgl
 
 pause
